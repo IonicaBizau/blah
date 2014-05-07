b2658a7169a738bc0d36719e6e40b075bed908fb
Ionică Bizău <bizauionica@yahoo.com>
2014-05-07 18:58:24 +0300

    Added __dirname

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ function generateLicense (licenseName) {
 
 
     return require ("fs")
-        .readFileSync ("./licenses/" + licenseName.toLowerCase() + ".txt")
+        .readFileSync (__dirname + "/licenses/" + licenseName.toLowerCase() + ".txt")
         .toString ()
         .replace ("[year]", new Date().getFullYear())
         .replace ("[fullname]", fullName)
